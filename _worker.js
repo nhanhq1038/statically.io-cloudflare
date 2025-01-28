3fd00ee7e40068566fd4179d6cf96ada53a4d409
nhanhq1038 <nhanhq1038@gmail.com>
2025-01-28 13:25:40 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="cdn.jsdelivr.net";
+        url.hostname="cdn.statically.io;
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
